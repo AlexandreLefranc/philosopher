ff6c33f07188cf5a0e4ee21b0cedce5340f384ea
Alexandre <alefranc.pro@mailo.com>
2022-05-30 23:41:49 +0200

    Routine writen, pseudo code of boss

---
+++ src/boss_job.c
@@ -0,0 +1,13 @@
+#include "philo.h"
+
+void	boss_job(/*fork_tab, philo_tab*/)
+{
+	while (running == 1 || all_philo_done(philo_tab) != 1)
+	{
+		// check_dead_philo();
+		// if (any_dead_philo == 1)
+			// running == 0;
+	}
+	// detach all threads
+	// free all
+}
--- src/routine.c
+++ src/routine.c
@@ -1,28 +1,48 @@
 #include "philo.h"
 
+struct s_arg
+{
+	int				*running;
+	pthread_mutex_t	handle_fork;
+	struct timeval	start_time;
+	t_input			*input;
+
+	size_t			id;
+	int				*left_fork;
+	int				*right_fork;
+	size_t			nb_meals;
+	struct timeval	last_meal;
+};
+
 void	*routine(void *arg)
 {
-	while (running == 1 && (nb_meals_max == -1 || nb_meals < nb_meal_max))
+	while (*(arg->running) == 1 && (arg->input->nb_meals_max == -1
+			|| arg->nb_meals < arg->input->nb_meal_max))
 	{
-		if (/*both forks available*/)
+		if (arg->left_fork == FREE && arg->right_fork == FREE)
 		{
-			// lock handle_forks
-			// if (both forks still available)
-				// take left_fork
-				// take right_fork
-				// unlock handle_forks
-			// else
-				// Unlock handle_forks
-				// continue
-			// set time last meal to now
-			// increment nb_meals
-			// print eating
-			// sleep for time2eat
-			// release left_fork
-			// release right_fork
-
-			// print sleeping
-			// sleep for time2sleep
+			pthread_mutex_lock(arg->handle_fork);
+			if (arg->left_fork == FREE && arg->right_fork == FREE)
+			{
+				arg->left_fork == USED;
+				print_taking_fork(arg);
+				arg->right_fork == USED;
+				print_taking_fork(arg);
+				pthread_mutex_unlock(arg->handle_fork);
+			}
+			else
+			{
+				pthread_mutex_unlock(arg->handle_fork);
+				continue ;
+			}
+			arg->last_meal = gettimeofday(arg->last_meal, NULL);
+			arg->nb_meals++;
+			print_eating(arg);
+			usleep(arg->input->time2eat * 1000);
+			arg->left_fork == FREE;
+			arg->right_fork == FREE;
+			print_sleeping(arg);
+			usleep(arg->input->time2sleep * 1000);
 		}
 	}
 }
